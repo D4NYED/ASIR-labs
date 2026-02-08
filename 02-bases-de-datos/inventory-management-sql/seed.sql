b6fe91c042aaa21948e6ae86264bebe73d9aa1e8
Daniel Espinosa <158766129+D4NYED@users.noreply.github.com>
2026-02-08 19:00:10 +0100

    feat: add sample inventory data

---
+++ 02-bases-de-datos/inventory-management-sql/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO Articulos (Descripcion, Precio, Inventario) VALUES
+('Lavadora Bosch', 499.99, 5),
+('Frigorifico LG', 899.99, 3),
+('Microondas Samsung', 120.00, 15),
+('Televisor Sony', 699.99, 7);
