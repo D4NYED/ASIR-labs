2d988bc11750eb697d305acc21c29ffe74179f20
Daniel Espinosa <158766129+D4NYED@users.noreply.github.com>
2026-02-08 18:42:59 +0100

    feat: add check constraints and defaults to schema

--- 02-bases-de-datos/inventory-database-schema/schema.sql
+++ 02-bases-de-datos/inventory-database-schema/schema.sql
@@ -4,6 +4,6 @@ USE TiendaMuebles;
 CREATE TABLE Productos (
     idproducto INT PRIMARY KEY AUTO_INCREMENT,
     descripcion VARCHAR(255) NOT NULL,
-    cantidad INT NOT NULL,
-    precio DECIMAL(10, 2) NOT NULL
+    cantidad INT NOT NULL DEFAULT 0 CHECK (cantidad >= 0),
+    precio DECIMAL(10,2) NOT NULL CHECK (precio >= 0)
 );
